426600d44b467e89ae93107876fc1f6fa35bf6b0
Bintang Pradana Erlangga Putra <bintang.pradana@privyid.tech>
2023-12-06 14:23:03 +0700

    feat: add encryption

---
+++ ducttape/main.py
@@ -0,0 +1,41 @@
+import argparse
+import sys
+
+import ducttape
+
+
+def main():
+    args = _parse_args()
+    output_file = _open_output(args)
+    output = ducttape.script(
+        args.script,
+        add_python_modules=args.add_python_module,
+        add_python_paths=args.add_python_path,
+        python_binary=args.python_binary,
+        copy_shebang=args.copy_shebang,
+        public_key=args.public_key,
+    )
+    output_file.write(output)
+
+
+def _open_output(args):
+    if args.output_file is None:
+        return sys.stdout
+    else:
+        return open(args.output_file, "w")
+
+
+def _parse_args():
+    parser = argparse.ArgumentParser()
+    parser.add_argument("script")
+    parser.add_argument("--add-python-module", action="append", default=[])
+    parser.add_argument("--add-python-path", action="append", default=[])
+    parser.add_argument("--python-binary")
+    parser.add_argument("--output-file")
+    parser.add_argument("--copy-shebang", action="store_true")
+    parser.add_argument("--public-key")
+    return parser.parse_args()
+
+
+if __name__ == "__main__":
+    main()
--- setup.py
+++ setup.py
@@ -3,23 +3,26 @@
 import os
 from setuptools import setup
 
+
 def read(fname):
     return open(os.path.join(os.path.dirname(__file__), fname)).read()
 
+
 setup(
-    name='stickytape',
-    version='0.2.1',
-    description='Convert Python packages into a single script',
+    name="ducttape",
+    version="0.0.1",
+    description="Convert Python packages into a single script",
     long_description=read("README.rst"),
-    author='Michael Williamson',
-    author_email='mike@zwobble.org',
-    url='https://github.com/mwilliamson/stickytape',
-    packages=['stickytape'],
-    python_requires='>=3.5',
+    author="Michael Williamson, Bintang Pradana Erlangga Putra",
+    author_email="mike@zwobble.org",
+    url="https://github.com/bpradana/ducttape",
+    packages=["ducttape"],
+    install_requires=["astor", "PyCryptodome"],
+    python_requires=">=3.5",
     license="BSD-2-Clause",
     entry_points={
         "console_scripts": [
-            "stickytape=stickytape.main:main"
-        ]
+            "ducttape=ducttape.main:main",
+        ],
     },
 )
